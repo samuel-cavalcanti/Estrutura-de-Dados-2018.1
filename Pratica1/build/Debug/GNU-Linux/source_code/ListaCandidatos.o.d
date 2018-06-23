50094e07e9ec14661019d38ad2c3877131fb529a
samuel-cavalcanti <scavalcanti111@gmail.com>
2018-06-23 14:15:45 -0300

    up pratica6

--- Pratica1/build/Debug/GNU-Linux/source_code/ListaCandidatos.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/GNU-Linux/source_code/ListaCandidatos.o: \
- source_code/ListaCandidatos.cpp source_code/ListaCandidatos.h \
- source_code/NoCandidato.h source_code/Candidato.h
-
-source_code/ListaCandidatos.h:
-
-source_code/NoCandidato.h:
-
-source_code/Candidato.h:
--- Pratica3/build/Debug/GNU-Linux/main.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU-Linux/main.o: main.cpp Test.h
-
-Test.h:
